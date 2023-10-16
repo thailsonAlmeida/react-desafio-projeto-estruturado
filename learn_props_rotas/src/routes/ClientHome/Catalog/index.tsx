97d143668f2b5219e3cfd13b66e2c1637545e0fd
Thailson Almeida <103151779+thailsonAlmeida@users.noreply.github.com>
2023-10-16 16:56:16 -0300

    Liks, catalog, product details

--- learn_props_rotas/src/routes/ClientHome/Catalog/index.tsx
+++ learn_props_rotas/src/routes/ClientHome/Catalog/index.tsx
@@ -1,33 +1,8 @@
 import ButtonNextPage from "../../../components/ButtonNextPage";
 import CatalogCard from "../../../components/CatalogCard";
 import SearchBar from "../../../components/SearchBar";
-import { ProductDTO } from "../../../models/product";
-import './styles.css';
-
-
-const product: ProductDTO = {
-  id: 2,
-  name: "Smart TV",
-  description:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do   eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim   ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut  aliquip ex ea commodo consequat. Duis aute irure dolor in  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  imgUrl:
-    "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg",
-  price: 2598.69,
-  categories: [
-    {
-      id: 2,
-      name: "Eletronicos",
-    },
-    {
-      id: 3,
-      name: "Computadores",
-    },
-    {
-      id: 4,
-      name: "Importados",
-    },
-  ],
-};
+import * as productService from "../../../services/product-service";
+import "./styles.css";
 
 export default function Catalog() {
   return (
@@ -35,13 +10,9 @@ export default function Catalog() {
       <section id="catalog-section" className="dsc-container">
         <SearchBar />
         <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
+          {productService.findAll().map((product) => (
+            <CatalogCard key={product.id} product={product} />
+          ))}
         </div>
         <ButtonNextPage />
       </section>
--- learn_props_rotas/src/routes/ProductDetails/index.tsx
+++ learn_props_rotas/src/routes/ProductDetails/index.tsx
@@ -1,42 +1,29 @@
 import ButtonInversa from "../../components/ButtonInverse";
 import ButtonPrimary from "../../components/ButtonPrimary";
 import ProductDetailsCard from "../../components/ProductDetailsCard";
-import { ProductDTO } from "../../models/product";
 import "./styles.css";
+import * as produtService from '../../services/product-service';
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 
-
-const product: ProductDTO = {
-  id: 2,
-  name: "Smart TV",
-  description:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do   eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim   ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut  aliquip ex ea commodo consequat. Duis aute irure dolor in  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  imgUrl:
-    "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg",
-  price: 2598.69,
-  categories: [
-    {
-      id: 2,
-      name: "Eletronicos",
-    },
-    {
-      id: 3,
-      name: "Computadores",
-    },
-    {
-      id: 4,
-      name: "Importados",
-    },
-  ],
-};
 
 export default function ProductDetails() {
+  const params = useParams();
+
+  const product = produtService.findById(Number(params.productId));
+
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
-        <ProductDetailsCard product={product} />
+        {
+          product &&
+          <ProductDetailsCard product={product} />
+        }        
         <div className="dsc-btn-page-container">
           <ButtonPrimary text="Comprar" />
-          <ButtonInversa text="Listar" />
+          <Link to={'/'}>
+            <ButtonInversa text="Início" />
+          </Link>
         </div>
       </section>
     </main>
