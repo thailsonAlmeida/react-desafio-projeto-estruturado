da7566e633f53af0f37fa8373b1d888b641b598b
Thailson Almeida <103151779+thailsonAlmeida@users.noreply.github.com>
2023-10-19 12:31:33 -0300

    Cart clear

--- 4-learn_local-storage/src/routes/ProductDetails/index.tsx
+++ 4-learn_local-storage/src/routes/ProductDetails/index.tsx
@@ -3,6 +3,7 @@ import ButtonPrimary from "../../components/ButtonPrimary";
 import ProductDetailsCard from "../../components/ProductDetailsCard";
 import "./styles.css";
 import * as produtService from '../../services/product-service';
+import * as cartService from '../../services/cart-service'; 
 import { useParams } from "react-router";
 import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
@@ -28,6 +29,14 @@ export default function ProductDetails() {
       });      
   }, []);
 
+  function handleBuyClick(){
+    if(product){
+      cartService.addProduct(product);
+      navigate('/cart');
+    }
+    
+  }
+
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
@@ -36,7 +45,8 @@ export default function ProductDetails() {
           <ProductDetailsCard product={product} />
         }        
         <div className="dsc-btn-page-container">
-          <ButtonPrimary text="Comprar" />
+          <div onClick={handleBuyClick}><ButtonPrimary text="Comprar" /></div>
+          
           <Link to={'/'}>
             <ButtonInversa text="Início" />
           </Link>
