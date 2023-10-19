0025b4ca6bb6d9d1753228c83ab7a8952687cb50
Thailson Almeida <103151779+thailsonAlmeida@users.noreply.github.com>
2023-10-19 15:20:31 -0300

    Search bar on Search event

---
+++ 5-learn_eventos-estados-globais/src/routes/ClientHome/Catalog/index.tsx
@@ -0,0 +1,41 @@
+import ButtonNextPage from "../../../components/ButtonNextPage";
+import CatalogCard from "../../../components/CatalogCard";
+import SearchBar from "../../../components/SearchBar";
+import { ProductDTO } from "../../../models/product";
+import * as productService from "../../../services/product-service";
+import { useEffect, useState } from "react";
+import "./styles.css";
+
+export default function Catalog() {
+
+  const [ products, setProducts ] = useState<ProductDTO[]>([]);
+
+  const [ productName, setProductName ] = useState('');
+
+  useEffect(() => {
+    productService.findPageRequest(0, productName, )
+      .then(response => {
+        setProducts(response.data.content);        
+      });
+  },[productName]);
+
+  function handleSearch(searchText: string){
+    setProductName(searchText);
+  }
+
+  return (
+    <main>
+      <section id="catalog-section" className="dsc-container">
+        <SearchBar onSearch={handleSearch} />
+
+        <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
+          {products.map((product) => (
+            <CatalogCard key={product.id} product={product} />
+          ))}
+        </div>
+        
+        <ButtonNextPage />
+      </section>
+    </main>
+  );
+}
---
+++ 5-learn_eventos-estados-globais/src/routes/ClientHome/ProductDetails/index.tsx
@@ -0,0 +1,54 @@
+import ButtonInversa from "../../../components/ButtonInverse";
+import ButtonPrimary from "../../../components/ButtonPrimary";
+import ProductDetailsCard from "../../../components/ProductDetailsCard";
+import "./styles.css";
+import * as produtService from "../../../services/product-service";
+import * as cartService from "../../../services/cart-service";
+import { useParams } from "react-router";
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { ProductDTO } from "../../../models/product";
+
+export default function ProductDetails() {
+  const params = useParams();
+
+  const navigate = useNavigate();
+
+  const [product, setProduct] = useState<ProductDTO>();
+
+  useEffect(() => {
+    produtService
+      .findById(Number(params.productId))
+      .then((response) => {
+        console.log(response.data);
+        setProduct(response.data);
+      })
+      .catch(() => {
+        navigate("/");
+      });
+  }, []);
+
+  function handleBuyClick() {
+    if (product) {
+      cartService.addProduct(product);
+      navigate("/cart");
+    }
+  }
+
+  return (
+    <main>
+      <section id="product-details-section" className="dsc-container">
+        {product && <ProductDetailsCard product={product} />}
+        <div className="dsc-btn-page-container">
+          <div onClick={handleBuyClick}>
+            <ButtonPrimary text="Comprar" />
+          </div>
+
+          <Link to={"/"}>
+            <ButtonInversa text="Início" />
+          </Link>
+        </div>
+      </section>
+    </main>
+  );
+}
