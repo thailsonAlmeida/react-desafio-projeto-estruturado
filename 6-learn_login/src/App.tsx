1fee38a6f82fb7e00bfb82b3601c5431b8cc0221
Thailson Almeida <103151779+thailsonAlmeida@users.noreply.github.com>
2023-10-21 18:08:33 -0300

    Place order

--- 6-learn_login/src/App.tsx
+++ 6-learn_login/src/App.tsx
@@ -46,11 +46,15 @@ export default function App() {
               <Route path="product-details/:productId" element={<ProductDetails />}/>
               <Route path="cart" element={<Cart />} />
               <Route path="login" element={<Login />} />
-              <Route path="confirmation/:orderId" element={<Confirmation />}/>
+              <Route path="confirmation/:orderId" 
+                element={
+                  <PrivateRoute>
+                    <Confirmation />
+                  </PrivateRoute>}
+              />
             </Route>
 
-            <Route
-              path="/admin/"
+            <Route path="/admin/"
               element={
                 <PrivateRoute roles={["ROLE_ADMIN"]}>
                   <Admin />
