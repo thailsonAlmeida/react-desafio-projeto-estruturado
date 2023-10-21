377c51b6b79b0573bb8e7cb57f9f726fe28d846d
Thailson Almeida <103151779+thailsonAlmeida@users.noreply.github.com>
2023-10-21 17:51:25 -0300

    Confirmation

--- 6-learn_login/src/App.tsx
+++ 6-learn_login/src/App.tsx
@@ -15,6 +15,7 @@ import { AccessTokenPayloadDTO } from "./models/auth";
 import { ContextToken } from "./utils/context-token";
 import * as authService from "./services/auth-services";
 import * as cartService from "./services/cart-service";
+import Confirmation from "./routes/ClientHome/Confirmation";
 
 export default function App() {
   const [contextCartCount, setContextCartCount] = useState<number>(0);
@@ -42,12 +43,10 @@ export default function App() {
             <Route path="/" element={<ClientHome />}>
               <Route index element={<Catalog />} />
               <Route path="catalog" element={<Catalog />} />
-              <Route
-                path="product-details/:productId"
-                element={<ProductDetails />}
-              />
+              <Route path="product-details/:productId" element={<ProductDetails />}/>
               <Route path="cart" element={<Cart />} />
               <Route path="login" element={<Login />} />
+              <Route path="confirmation/:orderId" element={<Confirmation />}/>
             </Route>
 
             <Route
