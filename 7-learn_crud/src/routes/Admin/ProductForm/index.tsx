27fce3c867caea3131d34540610c617d7c114259
Thailson Almeida <103151779+thailsonAlmeida@users.noreply.github.com>
2023-10-25 10:32:33 -0300

    formData, form products

--- 7-learn_crud/src/routes/Admin/ProductForm/index.tsx
+++ 7-learn_crud/src/routes/Admin/ProductForm/index.tsx
@@ -1,6 +1,42 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Link } from "react-router-dom";
 import "./styles.css";
+import { useState } from "react";
+import FormInput from "../../../components/FormInput";
+import * as forms from "../../../utils/forms";
 
 export default function ProductForm() {
+  
+  const [formData, setFormData] = useState<any>({
+    name: {
+      value: "",
+      id: "name",
+      name: "name",
+      type: "text",
+      placeholder: "Nome",
+    },
+    price: {
+      value: "",
+      id: "price",
+      name: "price",
+      type: "number",
+      placeholder: "Preço",
+    },
+    imgUrl: {
+      value: "",
+      id: "imgUrl",
+      name: "imgUrl",
+      type: "text",
+      placeholder: "Imagem",
+    },
+  });
+
+  function handleInputChange(event: any) {
+    const value = event.target.value;
+    const name = event.target.name;
+    setFormData(forms.update(formData, name, value));
+  }
+  
   return (
     <main>
       <section id="product-form-section" className="dsc-container">
@@ -9,47 +45,36 @@ export default function ProductForm() {
             <h2>Dados do produto</h2>
             <div className="dsc-form-controls-container">
               <div>
-                <input
-                  className="dsc-form-control"
-                  type="text"
-                  placeholder="Nome"
+                <FormInput
+                  { ...formData.name }                  
+                  className="dsc-form-control"                  
+                  onChange={handleInputChange}
                 />
               </div>
               <div>
-                <input
-                  className="dsc-form-control"
-                  type="text"
-                  placeholder="Preço"
+                <FormInput
+                  { ...formData.price }                  
+                  className="dsc-form-control"                  
+                  onChange={handleInputChange}
                 />
               </div>
               <div>
-                <input
-                  className="dsc-form-control"
-                  type="text"
-                  placeholder="Imagem"
+                <FormInput
+                  { ...formData.imgUrl }                  
+                  className="dsc-form-control"                  
+                  onChange={handleInputChange}
                 />
-              </div>
-              <div>
-                <select className="dsc-form-control dsc-select" required>
-                  <option value="" disabled selected>
-                    Categorias
-                  </option>
-                  <option value="1">Valor 1</option>
-                  <option value="2">Valor 2</option>
-                </select>
-              </div>
-              <div>
-                <textarea
-                  className="dsc-form-control dsc-textarea"
-                  placeholder="Descrição"
-                ></textarea>
-              </div>
+              </div>              
+          
             </div>
 
             <div className="dsc-product-form-buttons">
-              <button type="reset" className="dsc-btn dsc-btn-white">
-                Cancelar
-              </button>
+              <Link to="/admin/products">
+                <button type="reset" className="dsc-btn dsc-btn-white">
+                  Cancelar
+                </button>
+              </Link>
+              
               <button type="submit" className="dsc-btn dsc-btn-blue">
                 Salvar
               </button>
