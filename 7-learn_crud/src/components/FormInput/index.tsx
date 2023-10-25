9fb9787c100c741ff42e40b66f6f0f6e06c70307
Thailson Almeida <103151779+thailsonAlmeida@users.noreply.github.com>
2023-10-25 09:32:07 -0300

    Form Update

--- 7-learn_crud/src/components/FormInput/index.tsx
+++ 7-learn_crud/src/components/FormInput/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export default function FormInput(props: any){
     
--- 7-learn_crud/src/routes/ClientHome/Login/index.tsx
+++ 7-learn_crud/src/routes/ClientHome/Login/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useContext, useState } from "react";
 import * as authService from "../../../services/auth-services";
+import * as forms from "../../../utils/forms";
 import "./styles.css";
 import { useNavigate } from "react-router-dom";
 import { ContextToken } from "../../../utils/context-token";
@@ -51,7 +52,7 @@ export default function Login() {
   function handleInputChange(event: any) {
     const value = event.target.value;
     const name = event.target.name;
-    setFormData({ ...formData, [name]: {...formData[name], value: value} });
+    setFormData(forms.update(formData, name, value));
   }
 
   return (
